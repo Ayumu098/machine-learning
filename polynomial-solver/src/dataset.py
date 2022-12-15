b5f2e3ad50f2fcbef3573b20961f6c954691bb6f
Ayumu098 <stephen.singer.098@gmail.com>
2022-12-16 00:23:34 +0800

    Use numpy for Dataset

--- polynomial-solver/src/dataset.py
+++ polynomial-solver/src/dataset.py
@@ -1,38 +1,28 @@
-from numpy import genfromtxt
-from dataclasses import dataclass
-from typing import Optional, List
+from numpy import genfromtxt, array
+from dataclasses import dataclass, field
 
 @dataclass
 class Dataset():
-    """Contains the input and output values observed from a function.
+    """Contains the input and output values observed from a phenomenon.
     """
+    
+    csv_source: str
+    input_columns:  tuple[int] = tuple([0])
+    output_columns: tuple[int] = tuple([1])
+    input:  array = field(init=False)
+    output: array = field(init=False)
 
-    input:  list = Optional[List[float]]
-    output: list = Optional[List[float]]
-
-    def __init__(self, csv_source: str):
-        """Loads the input and output pairs from a .csv document filepath.
-
-        Args:
-            csv_source (str): Filepath to a Nx2 .csv file containing the 
-            x, y (input-output) pairs for a function.
+    def __post_init__(self):
+        """Loads the input and output pairs from provided csv filepath
         """
 
-        self.load(csv_source=csv_source)
+        # Extract all data past the header row
+        csv_contents = genfromtxt(self.csv_source, delimiter=',')[1:]
 
-    def load(self, csv_source: str):
-        """Loads the input and output pairs from a .csv document filepath.
+        # Set input and output pairs as (N, d) shaped numpy matrices
+        self.input  = csv_contents[:, self.input_columns ].reshape(-1, 1)
+        self.output = csv_contents[:, self.output_columns].reshape(-1, 1)
 
-        Args:
-            csv_source (str): Filepath to a Nx2 .csv file containing the 
-            x, y (input-output) pairs for a phenomenon to be modelled.
-        """
-
-        # Reads the rows past the header of the csv file as two tuples
-        input_output_pair = zip(*genfromtxt(csv_source, delimiter=',')[1:])
-
-        # Load the input and outputs as type list for convenience
-        self.input, self.output = map(list, input_output_pair)
-
-        assert len(self.input) == len(self.output),\
-            f"Input count in {csv_source} must match Output count"
+        # Format input and output as (N, 1) shaped numpy arrays
+        self.input  = array(self.input).reshape(-1, 1)
+        self.output = array(self.input).reshape(-1, 1)
